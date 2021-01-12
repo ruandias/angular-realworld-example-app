cf498c521349e1584259b607cf0719ee19608558
ruandias <ruan_ndias@hotmail.com>
2021-01-12 13:15:27 -0300

    adicionado teste de edição de post

--- cypress/integration/post.spec.js
+++ cypress/integration/post.spec.js
@@ -3,13 +3,23 @@ describe('Post', () => {
     cy.login('testecypress@testecypress.com', 'testecypress')
   })
   it('Novo', () => {
-    const tit = 'Cypress E2E'
+    const title = 'Cypress E2E'
     cy.contains('New Article').click()
     cy.location('pathname').should('equal','/editor')
-    cy.get('[formcontrolname=title]').type(tit)
+    cy.get('[formcontrolname=title]').type(title)
     cy.get('[formcontrolname=description]').type('Ponta a Ponta')
     cy.get('[formcontrolname=body]').type('Agilidade', 'Qualidade')
     cy.contains('Publish Article').click()
-    cy.get('h1').contains(tit)
+    cy.get('h1').contains(title)
+  })
+  it('Editar', () => {
+    cy.contains('testecypress').click()
+    cy.location('pathname').should('contains', '/profile')
+    cy.get('.article-preview').get('h1').first().click()
+    cy.contains('Edit Article').click()
+    cy.get('[formcontrolname=body]').clear()
+    cy.get('[formcontrolname=body]').type('Economia')
+    cy.contains('Publish Article').click()
+    cy.contains('Economia')
   })
 })
